922cd968ca289ddb898f7e32340a81bb6e89ad8a
Raúl Marrero <113340217+raulm1906@users.noreply.github.com>
2026-04-04 12:38:44 +0100

    Creados archivos de configuracion para servidor y produccion

--- web_deii_project/settings/base.py
+++ web_deii_project/settings/base.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
-
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,12 +18,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s0c)kxc)b_#+ixci(!pd3ol@csii2l(n*h*k5-w)x86k2-2pkz'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
