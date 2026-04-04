a8c2107931608782b077450e82c37812154c7dea
Raúl Marrero <113340217+raulm1906@users.noreply.github.com>
2026-04-04 11:16:49 +0100

    gitignore y configuracion de conexion a base de datos

--- web_deii_project/settings.py
+++ web_deii_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -75,9 +76,12 @@ WSGI_APPLICATION = 'web_deii_project.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": os.environ.get("PGSERVICE"),
+            "passfile": os.environ.get("PGPASSFILE"),
+        },
     }
 }
 
