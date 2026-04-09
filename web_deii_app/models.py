410230d0bb543d80ab7dfaf0b24c07eba6e146a4
Jesus Toro <jesus.tdd03@gmail.com>
2026-04-09 15:37:46 +0100

    Created table faq

--- web_deii_app/models.py
+++ web_deii_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Faq(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=255)
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
