11810071063d24cb3f5bfa7ab8019a0d20e510c3
Jesus Toro <jesus.tdd03@gmail.com>
2026-04-09 16:08:15 +0100

    Implemented class Tags

--- web_deii_app/models.py
+++ web_deii_app/models.py
@@ -2,6 +2,10 @@ import uuid
 
 from django.db import models
 
+class Tags(models.Model):
+    id = models.AutoField(primary_key=True)
+    tag = models.CharField(max_length=255, blank=False, null=False)
+
 class Faq(models.Model):
     id = models.AutoField(primary_key=True)
     category = models.CharField(max_length=255, blank=False, null=False)
