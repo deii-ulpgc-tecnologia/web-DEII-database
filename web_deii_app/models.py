ac8f8f7ef1f60ebc3aaedeb3b7011c1451914500
Jesus Toro <jesus.tdd03@gmail.com>
2026-04-09 15:58:08 +0100

    Updated faq fields

--- web_deii_app/models.py
+++ web_deii_app/models.py
@@ -1,7 +1,9 @@
+import uuid
+
 from django.db import models
 
 class Faq(models.Model):
     id = models.AutoField(primary_key=True)
-    category = models.CharField(max_length=255)
-    question = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, blank=False, null=False)
+    question = models.CharField(max_length=255, blank=False, null=False)
     answer = models.TextField()
