72e207a8356b7b7fe19787cc08e27ab0755c1413
Jesus Toro <jesus.tdd03@gmail.com>
2026-04-09 16:14:24 +0100

    Implemented class Subjects

--- web_deii_app/models.py
+++ web_deii_app/models.py
@@ -2,6 +2,15 @@ import uuid
 
 from django.db import models
 
+
+class Subjects(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    degree = models.CharField(max_length=255, blank=False, null=False)
+    year = models.IntegerField(blank=False, null=False)
+    semester = models.IntegerField(blank=False, null=False)
+    area = models.CharField(max_length=255, blank=False, null=False)
+
 class Tags(models.Model):
     id = models.AutoField(primary_key=True)
     tag = models.CharField(max_length=255, blank=False, null=False)
