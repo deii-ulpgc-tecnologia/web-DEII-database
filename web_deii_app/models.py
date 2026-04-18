71b0f4d1db8431162ae35412982f533dac2c42a7
Raúl Marrero <113340217+raulm1906@users.noreply.github.com>
2026-04-18 10:37:12 +0100

    Created users models and usermanage functions

--- web_deii_app/models.py
+++ web_deii_app/models.py
@@ -1,9 +1,50 @@
 import uuid
-
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 
-# TODO users
+class UserManager(BaseUserManager):
+    def create_user(self, email, password, is_comunicacion=False, is_estudios=False, **extra_fields):
+        if not email:
+            raise ValueError('El email es obligatorio.')
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            is_comunicacion=is_comunicacion,
+            is_estudios=is_estudios,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_comunicacion', True)
+        extra_fields.setdefault('is_estudios', True)
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(unique=True, max_length=255, blank=False, null=False)
+    email = models.CharField(unique=True, max_length=255, blank=False, null=False)
+    password = models.CharField(max_length=255, blank=False, null=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_comunicacion = models.BooleanField(default=False)
+    is_estudios = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        db_table = 'user'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
 
 class Subjects(models.Model):
     id = models.AutoField(primary_key=True)
@@ -24,13 +65,13 @@ class Files(models.Model):
     uploader = models.CharField(max_length=255, blank=False, null=False)
     file = models.FileField(upload_to=file_path, blank=False, null=False, unique=True)
     is_active = models.BooleanField(default=False)
-    # TODO approver_id
+    approver_id = models.ForeignKey('User', related_name='files_id', on_delete=models.SET_NULL, null=True)
     publish_date = models.DateField(blank=False, null=False)
 
 
 class NewsPosts(models.Model):
     id = models.AutoField(primary_key=True)
-    # todo user_id
+    user_id = models.ForeignKey('User', related_name='posts_id', on_delete=models.SET_NULL, null=True)
     title = models.CharField(max_length=255, blank=False, null=False)
     content = models.TextField(blank=False, null=False)
     is_active = models.BooleanField(default=False)
--- web_deii_project/settings/base.py
+++ web_deii_project/settings/base.py
@@ -98,6 +98,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'web_deii_app.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
