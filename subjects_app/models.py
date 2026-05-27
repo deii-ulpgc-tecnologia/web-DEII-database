177ed4c598277a0a6e93d87b12c067efeee9d641
Jesus Toro <jesus.tdd03@gmail.com>
2026-05-27 18:45:51 +0100

    Added implemented year field as an enum

--- subjects_app/models.py
+++ subjects_app/models.py
@@ -3,16 +3,26 @@ from django.db import models
 # Create your models here.
 
 class DegreeEnum(models.TextChoices):
-    gii = "GII", "Ingeniería Informática"
-    gcid = "GCID", "Ciencia e Ingeniería de Datos"
-    gifm = "GIFM", "Ingeniería Física y Matemática"
+    GII = "GII", "Ingeniería Informática"
+    GCID = "GCID", "Ciencia e Ingeniería de Datos"
+    GIFM = "GIFM", "Ingeniería Física y Matemática"
+
+class YearEnum(models.IntegerChoices):
+    YEAR1 = 1, "primero"
+    YEAR2 = 2, "segundo"
+    YEAR3 = 3, "tercero"
+    YEAR4 = 4, "cuarto"
+    YEAR5 = 5, "quinto"
+    YEAR6 = 6, "sexto"
+    MASTER = 7, "master"
+    PHD = 8, "phd"
 
 class Subject(models.Model):
     id = models.IntegerField(blank=False, null=False, primary_key=True)
     name = models.CharField(max_length=255, blank=False, null=False)
     code = models.CharField(max_length=255, blank=False, null=False)
-    degree = models.CharField(max_length=255, choices=DegreeEnum, default=DegreeEnum.gii, blank=False, null=False)
-    year = models.IntegerField(blank=False, null=False)
+    degree = models.CharField(max_length=255, choices=DegreeEnum, default=DegreeEnum.GII, blank=False, null=False)
+    year = models.IntegerField(choices=YearEnum, default=YearEnum.YEAR1, blank=False, null=False)
     semester = models.IntegerField(blank=False, null=False)
     area = models.CharField(max_length=255, blank=False, null=False)
 
