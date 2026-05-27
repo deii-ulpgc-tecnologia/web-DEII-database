ba454bc9b9f2b37b9f82ab984af83ab60e4eeba0
Jesus Toro <jesus.tdd03@gmail.com>
2026-05-27 18:39:35 +0100

    Added code field and implemented degree field as an enum

--- subjects_app/models.py
+++ subjects_app/models.py
@@ -1,10 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class DegreeEnum(models.TextChoices):
+    gii = "GII", "Ingeniería Informática"
+    gcid = "GCID", "Ciencia e Ingeniería de Datos"
+    gifm = "GIFM", "Ingeniería Física y Matemática"
+
 class Subject(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.IntegerField(blank=False, null=False, primary_key=True)
     name = models.CharField(max_length=255, blank=False, null=False)
-    degree = models.CharField(max_length=255, blank=False, null=False)
+    code = models.CharField(max_length=255, blank=False, null=False)
+    degree = models.CharField(max_length=255, choices=DegreeEnum, default=DegreeEnum.gii, blank=False, null=False)
     year = models.IntegerField(blank=False, null=False)
     semester = models.IntegerField(blank=False, null=False)
     area = models.CharField(max_length=255, blank=False, null=False)
