66172a1369d3d335942263df40360ab3d2e95fcc
Jerónimo Falcón <jfalcondavila42@gmail.com>
2026-06-30 12:38:11 +0100

    feat (news): create model newsimage and add field cover_image to newspost

--- news_app/models.py
+++ news_app/models.py
@@ -1,7 +1,14 @@
+import uuid
 from django.db import models
 from django.conf import settings
 
+
 # Create your models here.
+def news_image_path(instance, filename):
+    ext = filename.split('.')[-1]
+    return f'news/{instance.post_id}.{instance.id}.{ext}'
+
+
 class NewsPost(models.Model):
     id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='posts_id', on_delete=models.SET_NULL, null=True)
@@ -11,6 +18,23 @@ class NewsPost(models.Model):
     pinned = models.BooleanField(default=False)
     publish_date = models.DateField(blank=False, null=False)
     edited_date = models.DateField(null=True)
+    cover_image = models.ForeignKey(
+        'NewsImage',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name='cover_of_news_post'
+    )
 
     def __str__(self):
         return self.title
+
+
+class NewsImage(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    post = models.ForeignKey(NewsPost, on_delete=models.CASCADE, related_name='images')
+    image = models.FileField(upload_to=news_image_path, blank=False, null=False)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.post.title} - {self.id}"
